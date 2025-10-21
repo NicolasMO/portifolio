7a57f68122a78d654299e3da4cae992abdd9e264
Nicolas Oliveira <nicolas_oliveira97@hotmail.com>
2025-10-20 21:03:45 -0300

    criacao da tela portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/header/Header'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import Portfolio from './pages/portfolio/Portfolio';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
         <Header />
         <Home />
         <About />
+        <Portfolio />
         </>
     )
 }
---
+++ src/pages/portfolio/Portfolio.jsx
@@ -0,0 +1,39 @@
+import styles from './Portfolio.module.css'
+
+function Portfolio() {
+
+    return (
+        <>
+            <section id="portfolio" className={styles.container}>
+                <div class={styles.portfolioInfo}>
+                    <h2>
+                        <span>Meu</span> Portfólio
+                    </h2>
+                    <p>Esses são alguns projetos e trabalhos que já realizei</p>
+                </div>
+                <div className={styles.cardGrid}>
+                    <div className={styles.card}>
+                        <img className={styles.cardImage} src="src\assets\retangulo.png"/>
+                        <div className={styles.cardContent}>
+                            <p>Projeto de marmitaria visando comercialização de marmitas personalizadas</p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.cardImage} src="src\assets\retangulo.png"/>
+                        <div className={styles.cardContent}>
+                            <p>Projeto de marmitaria visando comercialização de marmitas personalizadas</p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.cardImage} src="src\assets\retangulo.png"/>
+                        <div className={styles.cardContent}>
+                            <p>Projeto de marmitaria visando comercialização de marmitas personalizadas</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Portfolio
